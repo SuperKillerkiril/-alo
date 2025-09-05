7acb65fc8d02f947cd734fa0ebaf622ce0a3e76d
Adam <udalovbg7@gmail.com>
2025-09-05 23:25:28 +0400

    add swagger

--- ApiTestForAngularApp/Program.cs
+++ ApiTestForAngularApp/Program.cs
@@ -1,5 +1,6 @@
 using ApiTestForAngularApp.DataBase;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,22 +9,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ApiTestForAngularApp API",
+        Version = "v1",
+        Description = "api test for Angular App",
+    });
+});
 builder.Services.AddDbContext<ModelsContext>();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
-    app.MapOpenApi();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiTestForAngularApp v1");
+        c.RoutePrefix = "swagger"; //путь к сваггеру 
+    });
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 var scope = app.Services.CreateScope();
